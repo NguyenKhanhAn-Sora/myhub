b16e72c1a5b560f4183d0655b01fd06bca070fee
NguyenKhanhAn-Sora <soranguyen1211@gmail.com>
2025-09-22 19:20:53 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Button, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import React from "react";
 
 const App = () => {
   return (
-    <HStack>
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
-    </HStack>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Navigation Bar
+      </GridItem>
+      <Show when={"lg"}>
+        <GridItem
+          area={"aside"}
+          bg={"gold"}
+          display={{ base: "none", lg: "block" }}
+        >
+          Left Side Bar
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"cyan"}>
+        Main Items
+      </GridItem>
+    </Grid>
   );
 };
 
