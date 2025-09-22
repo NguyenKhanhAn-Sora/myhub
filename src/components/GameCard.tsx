2e3a951b741008a6f8159160e58f8dc896afdb22
NguyenKhanhAn-Sora <soranguyen1211@gmail.com>
2025-09-22 20:58:54 +0700

    build game card and created grid by responsive

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,30 @@
+import { Button, Card, Image, Text } from "@chakra-ui/react";
+import type { Game } from "./hooks/useGame";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card.Root maxW="sm" overflow="hidden">
+      <Image src={game.background_image} alt="Game List" />
+      <Card.Body gap="2">
+        <Card.Title>{game.name}</Card.Title>
+        <Card.Description>
+          This sofa is perfect for modern tropical spaces, baroque inspired
+          spaces.
+        </Card.Description>
+        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+          $450
+        </Text>
+      </Card.Body>
+      <Card.Footer gap="2">
+        <Button variant="solid">Buy now</Button>
+        <Button variant="ghost">Add to cart</Button>
+      </Card.Footer>
+    </Card.Root>
+  );
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,23 @@
-
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "./hooks/useGame";
-
-
-
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const {games, error} = useGame();
+  const { games, error } = useGame();
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      {/* <ul>
         {games.map((game) => (
           <li>{game.name}</li>
         ))}
-      </ul>
+      </ul> */}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap="10px">
+        {games.map((g) => (
+          <GameCard key={g.id} game={g} />
+        ))}
+      </SimpleGrid>
     </>
   );
 };
